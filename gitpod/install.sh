a4c99edbce543a4450ebe4006ec9688d117346b0
Gerard Braad <me@gbraad.nl>
2023-11-27 12:27:07 +0000

    Clone repos

--- gitpod/install.sh
+++ gitpod/install.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
 # clean workspace folder
-rm -rf /workspace/ubi8-gotoolset
-mkdir /workspace/ubi8-gotoolset
-ln -s /workspace/ubi8-gotoolset ~/Projects
-git init /workspace/ubi8-gotoolset
+rm -rf /workspace/crc-gotoolset
+mkdir /workspace/crc-gotoolset
+ln -s /workspace/crc-gotoolset ~/Projects
+git init /workspace/crc-gotoolset
 
-cd ~
+cd ~/Projects
+
+git clone git@github.com:crc-org/crc
+git clone git@github.com:crc-org/snc
+git clone git@github.com:crc-org/admin-helper
+git clone git@github.com:crc-org/routes-controller
+git clone git@github.com:crc-org/win32-background-launcher
 
 exit 0
